e71733c45f92356df5b79aa66a5e2936c5f0003e
Starford Gwala <starfordgwala@gmail.com>
2026-03-10 16:55:15 +0300

    added dropdown and removed model that wasnt working

--- streamlit-sharing-using-streamlit/app.py
+++ streamlit-sharing-using-streamlit/app.py
@@ -10,16 +10,9 @@ from sklearn.preprocessing import StandardScaler
 model = joblib.load("./model/decisiontree_classifier_baseline.pkl")
 
 # we have added this as part of class assignment
-nbcmodel = joblib.load("./model/naive_Bayes_classifier_optimum.pkl")
-knnmodel = joblib.load("./model/knn_classifier_optimum.pkl")
-rfcmodel = joblib.load("./model/random_forest_classifier_optimum.pkl")
 svmmodel = joblib.load("./model/support_vector_classifier_optimum.pkl")
 decisiontree_regressor_optimum = joblib.load('./model/decisiontree_regressor_optimum.pkl')
 label_encoders_1b = joblib.load('./model/label_encoders_1b.pkl')
-
-#confirm with Robbi about this
-scaler_five = joblib.load('./model/scaler_3.pkl')
-onehot_encoder_path = joblib.load('./model/onehot_encoder_3.pkl')
 
 
 # Streamlit page config
@@ -36,14 +29,14 @@ st.set_page_config(
 
 st.title("Dashboard")
 
+st.header("Chosen Customer Churn and Predict Profit")
+
 # -----------------------------
 # Tabs for different models
 # -----------------------------
-tab1, tab2, tab3, tab4 = st.tabs([
+tab1, tab2 = st.tabs([
     "Customer Churn",
-    "Predict Profit",
-    "Predict Lateness ( KNN )",
-    "Placeholder"
+    "Predict Profit"
 ])
 
 # -----------------------------
@@ -85,8 +78,16 @@ with tab2:
             placeholder="Choose an option...",
             )
         
+        
         branch_sub_county = st.text_input("Branch Sub-County", "e.g., Kilimani")
-        product_category_name = st.text_input("Product Category Name", "e.g., Meat-Based Dishes")
+        
+        product_category_name = st.selectbox(
+            "Select Product Cateogry",
+            options=["African Cultural Specials","Combination Plates","Fish Dishes","Fried Dishes","Legume-Based Dishes","Meat-Based Dishes","Rice Dishes","Soup/Stew Dishes","Staple Foods","Sweet Snacks/Desserts","Vegetable-Based Dishes"],
+            index=None,
+            placeholder="Choose an option...",
+            )
+        
         quantity_ordered = st.number_input("Quantity Ordered")
         payment_date = st.date_input("Payment Date", date(2030, 7, 6))
 
@@ -155,105 +156,3 @@ with tab2:
 
         st.success(f"Prediction Profit: {prediction_regressor}")
         #st.subheader(f"Predicted Percentage Profit per Unit: {prediction_regressor:.2f}%")
-
-
-
-# -----------------------------
-# KNN
-# -----------------------------
-with tab3:
-
-    st.header("Predict Lateness")
-
-    with st.form("predict_lateness"):
-
-        days_shipping_real = st.number_input("Days for shipping (real)")
-        days_shipping_scheduled = st.number_input("Days for shipment (scheduled)")
-        
-        # 1. Define your mapping
-        delivery_mapping = {
-             "Yes": 1,
-             "No": 0
-             }
-        # 2. Display the labels in the dropdown
-        delivery_selection = st.selectbox(
-            "Risk of Late Delivery",
-            options=list(delivery_mapping.keys()), # This shows ["Late", "On Time"]
-            index=None,
-            placeholder="Choose an option..."
-            )
-        order_item_quantity = st.number_input("Order Item Quantity")
-
-        sales = st.number_input("Sales")
-        order_profit_per_order = st.number_input("Order Profit Per Order")
-        
-        shipping_mode = st.selectbox(
-            "Shipping Mode",
-            options=["Standard Class", "First Class"],
-            index=None,
-            placeholder="Choose an option...",
-            )
-        submit_lateness_prediction = st.form_submit_button("Predict Lateness")
-
-    if submit_lateness_prediction:
-        # # Build the initial dictionary (mimicking your JSON request)
-        data = {
-            'Days for shipping (real)': int(days_shipping_real),
-            'Days for shipment (scheduled)': int(days_shipping_scheduled),
-            'Late_delivery_risk':delivery_selection,
-            'Order Item Quantity': int(order_item_quantity),
-            'Sales': int(sales),
-            'Order Profit Per Order': float(order_profit_per_order),
-            'Shipping Mode': shipping_mode
-            }
-        
-        
-
-        # # Convert to DataFrame
-        new_data = pd.DataFrame([data])
-
-        
-
-        # One-hot encode 'Shipping Mode'
-        encoded = onehot_encoder_path.transform(new_data[['Shipping Mode']])
-        encoded_df = pd.DataFrame(encoded, columns=onehot_encoder_path.get_feature_names_out(['Shipping Mode']))
-        new_data_preprocessed = pd.concat([new_data.drop('Shipping Mode', axis=1), encoded_df], axis=1)
-
-        # Scale the features
-        new_data_scaled = scaler_five.transform(new_data_preprocessed)
-
-        # Make predictions
-        predictionknn = knnmodel.predict(new_data_scaled)[0]
-
-        
-
-        st.write(col)
-        st.write(new_data[col])
-        st.write(label_encoders_1b[col].classes_)
-
-        st.success(f"Prediction: {predictionknn}")
-        # #st.subheader(f"Predicted Percentage Profit per Unit: {prediction_regressor:.2f}%")
-
-
-
-
-# -----------------------------
-# SALES MODEL FORM
-# -----------------------------
-with tab4:
-
-    st.header("Sales Forecast")
-
-    with st.form("sales_form"):
-
-        marketing_spend = st.number_input("Marketing Spend")
-        store_visits = st.number_input("Store Visits")
-
-        submit_sales = st.form_submit_button("Predict Sales")
-
-    if submit_sales:
-
-        X = np.array([[marketing_spend, store_visits]])
-        prediction = sales_model.predict(X)
-
-        st.success(f"Predicted Sales: {prediction[0]}")
